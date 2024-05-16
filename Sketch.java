4645c9bc7e26b4b5babc6110851ba5c810c6a6b7
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-05-16 18:50:04 -0400

    modified:   Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -4,6 +4,11 @@ public class Sketch extends PApplet {
 	
 	int intSunX = 50;
   int intSunY = 50;
+
+  int intHouseX = 250;
+  int intHouseY = 200;
+
+  int intGrassColour = 79;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -24,10 +29,14 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    background(43, 223, 255);
-    cordHouse(250, 200, 75, 100);
-    cordGrass(0, 300);
-    
+    background(43, 223, 255 );
+    cordHouse(intHouseX , intHouseY, 75, 100);
+    cordGrass(0, 300, 79);
+    textSize(72);
+    text(key, 50, 250);
+    cordSun(intSunX,intSunY, 50);
+
+    // 
     if (keyPressed){
       if (keyCode == UP) {
         intSunY--;
@@ -42,8 +51,35 @@ public class Sketch extends PApplet {
         intSunX++;
       }
     }
-    cordSun(intSunX,intSunY, 50);
-    ellipse(mouseX, mouseY, 50, 50);
+    if (keyPressed){
+      if (key == 'w') {
+        intHouseY--;
+      }
+      else if (key == 's') {
+        intHouseY++;
+      }
+      else if(key == 'a'){
+        intHouseX--;
+      }
+      else if(key == 'd'){
+        intHouseX++;
+      }
+    }
+
+    if(keyPressed) {
+      if(key == 't' ){
+        cordTree(mouseX, mouseY);
+      }
+    }
+
+    if(keyPressed){
+      if(key == 'm'){
+        cordMoon(intSunX, intSunY, 50);
+      }
+    }
+    if(mousePressed == true) {
+      intGrassColour = 150;
+    }
   }
 /**
  *  A method cordHouse that makes a house with parameters
@@ -64,15 +100,43 @@ public class Sketch extends PApplet {
     rect(x + width / 2 - 15, y + length / 2 + 10, 30, 40);
   }
 
-  private void cordGrass(int x1, int y1){
-    fill(80, 199, 40);
+  private void cordGrass(int x1, int y1, int intColourGrass){
+    fill(intColourGrass + 1, intColourGrass + 120, intColourGrass - 59);
     rect(x1, y1, 400, 100);
   }
-
+/**
+ * 
+ * @param x2
+ * @param y2
+ * @param size
+ */
   private void cordSun(int x2, int y2, int size){
     strokeWeight(1);
     fill(249, 255, 89);
     ellipse(x2, y2, size, size);
+  
+  }
+/**
+ * 
+ * @param treeX
+ * @param treeY
+ */
+  private void cordTree (int treeX, int treeY) {
+    fill(161, 100, 63);
+    rect(treeX, treeY, 25, 75);
+    fill(92, 224, 83);
+    ellipse(treeX + 25/2, treeY, 50, 50);
+  }
+  /**
+   * 
+   * @param moonX
+   * @param moonY
+   * @param size
+   */
+  private void cordMoon(int moonX, int moonY, int size){
+    strokeWeight(1);
+    fill(237, 236, 216);
+    ellipse(moonX, moonY, size, size);
     
   }
 }
