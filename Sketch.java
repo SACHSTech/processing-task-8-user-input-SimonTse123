fb6f01c30ad9d7243d33a81b9561e20a374fb505
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-05-24 13:17:31 -0400

    modified:   Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,9 +1,14 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	int intSunX = 50;
-  int intSunY = 50;
+	// Declaring all variables
+	float intSunX = 50;
+  float intSunY = 50;
+
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
 
   int intHouseX = 250;
   int intHouseY = 200;
@@ -13,7 +18,7 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
+	// Size of the screen
     size(400, 400);
   }
 
@@ -29,6 +34,7 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    // Original image
     background(43, 223, 255 );
     cordHouse(intHouseX , intHouseY, 75, 100);
     cordGrass(0, 300, 79);
@@ -36,21 +42,22 @@ public class Sketch extends PApplet {
     text(key, 50, 250);
     cordSun(intSunX,intSunY, 50);
 
-    // 
+    // Movement of the sun using boolean variables
     if (keyPressed){
-      if (keyCode == UP) {
+      if (upPressed) {
         intSunY--;
       }
-      else if (keyCode == DOWN) {
+      if (downPressed) {
         intSunY++;
       }
-      else if(keyCode == LEFT){
+      if(leftPressed){
         intSunX--;
       }
-      else if(keyCode == RIGHT){
+      if(rightPressed){
         intSunX++;
       }
     }
+    // Movement of the house using key inputs
     if (keyPressed){
       if (key == 'w') {
         intHouseY--;
@@ -65,28 +72,25 @@ public class Sketch extends PApplet {
         intHouseX++;
       }
     }
-
+    // Creation of a tree when key is pressed
     if(keyPressed) {
       if(key == 't' ){
         cordTree(mouseX, mouseY);
       }
     }
-
+    // Changing the colour of the sun
     if(keyPressed){
       if(key == 'm'){
         cordMoon(intSunX, intSunY, 50);
       }
     }
-    if(mousePressed == true) {
-      intGrassColour = 150;
-    }
   }
 /**
  *  A method cordHouse that makes a house with parameters
- * @param x
- * @param y
- * @param width
- * @param length
+ * @param x x is the x coordinate of the rectangular section of the house
+ * @param y y is the y coordinate of the rectangular section of the house
+ * @param width width is width of the house
+ * @param length length is the length of the house
  */
   private void cordHouse(int x, int y, int width, int length){
     strokeWeight(2);
@@ -99,27 +103,32 @@ public class Sketch extends PApplet {
     ellipse( x + width / 2, y + length / 2 + 10, 30, 30);
     rect(x + width / 2 - 15, y + length / 2 + 10, 30, 40);
   }
-
+/**
+ *  A method cordGrass that creates grass with parameters.
+ * @param x1 x1 is the x coordinate the grass is orginally drawn from
+ * @param y1 y1 is the y coordinate the grass is orignally drawn from
+ * @param intColourGrass a value that adjusts the colour of the grass
+ */
   private void cordGrass(int x1, int y1, int intColourGrass){
     fill(intColourGrass + 1, intColourGrass + 120, intColourGrass - 59);
     rect(x1, y1, 400, 100);
   }
 /**
- * 
- * @param x2
- * @param y2
- * @param size
+ *  A method cordSun that creates a sun at a given coordinate and size
+ * @param x2 x2 is the x coordinate of the sun
+ * @param y2 y2 is the y coordinate of the sun
+ * @param size size refers to the size of the sun
  */
-  private void cordSun(int x2, int y2, int size){
+  private void cordSun(float x2, float y2, int size){
     strokeWeight(1);
     fill(249, 255, 89);
     ellipse(x2, y2, size, size);
   
   }
 /**
- * 
- * @param treeX
- * @param treeY
+ *  A method cordTree that creates a tree at a coordinate
+ * @param treeX treeX is the x coordinate the rectangular section of the tree is drawn from
+ * @param treeY treeY is the y coordinate the rectangular section of the tree is drawn from
  */
   private void cordTree (int treeX, int treeY) {
     fill(161, 100, 63);
@@ -128,16 +137,74 @@ public class Sketch extends PApplet {
     ellipse(treeX + 25/2, treeY, 50, 50);
   }
   /**
-   * 
-   * @param moonX
-   * @param moonY
-   * @param size
+   *  A method cordMoon that creates a moon at a coordinate and size
+   * @param moonX moonX is the x coordinate the moon is drawn from
+   * @param moonY moonY is the y coordinate the moon is drawn from
+   * @param size size refers the the size of the moon
    */
-  private void cordMoon(int moonX, int moonY, int size){
+  private void cordMoon(float moonX,float moonY, int size){
     strokeWeight(1);
     fill(237, 236, 216);
     ellipse(moonX, moonY, size, size);
     
+  }
+  /*
+   * A method mousePressed that changes the background's colour when the user presses the mouse
+   */
+  public void mousePressed(){
+    background(64, 81, 194);
+    cordHouse(intHouseX , intHouseY, 75, 100);
+    cordGrass(0, 300, 79);
+    textSize(72);
+    text(key, 50, 250);
+    cordSun(intSunX,intSunY, 50);
+  }
+/*
+ *  A method mouseDragged that changes the background's colour when the user drags the mouse
+ */
+  public void mouseDragged(){
+    background(14, 10, 89);
+    cordHouse(intHouseX , intHouseY, 75, 100);
+    cordGrass(0, 300, 79);
+    textSize(72);
+    text(key, 50, 250);
+    cordSun(intSunX,intSunY, 50);
+    fill(83, 92, 55);
+    ellipse(mouseX, mouseY, 50, 25);
+  }
+/**
+ * A method that changes the boolean value of variables when certain keys are pressed
+ */
+  public void keyPressed() {
+    if (keyCode == UP) {
+      upPressed = true;
+    }
+    else if (keyCode == DOWN) {
+      downPressed = true;
+    }
+    else if (keyCode == LEFT) {
+      leftPressed = true;
+    }
+    else if (keyCode == RIGHT) {
+      rightPressed = true;
+    }
+  }
+/**
+ *  A method keyReleased that changes the boolean value of variables when certain keys are released
+ */
+  public void keyReleased() {
+    if (keyCode == UP) {
+      upPressed = false;
+    }
+    else if (keyCode == DOWN) {
+      downPressed = false;
+    }
+    else if (keyCode == LEFT) {
+      leftPressed = false;
+    }
+    else if (keyCode == RIGHT) {
+      rightPressed = false;
+    }
   }
 }
 
